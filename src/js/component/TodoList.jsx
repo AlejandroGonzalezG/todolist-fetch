5f0b7b8a3e24bd5a0a23dab976ec56965a8bd93f
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-13 22:15:52 +0000

    avance todolist

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { MdOutlineClose } from 'react-icons/md';
 
 
 
@@ -10,23 +11,47 @@ const estiloInput = {
     width: "50%",
 }
 
+const estiloDivMap = {
+    margin: "3px"
+}
+
 
 const TodoList = (props) => {
 
-    
+
 
     const estiloSpan = {
         width: "50%",
-        display: props.span ,
+        display: props.span,
+    }
+
+    const eliminar = (num) => {
+        const index = props.objeto[num];
+        if (num > -1) { 
+            props.objeto.splice(num, 1);
+            num = num-1
+            console.log(props.objeto)
+        }
     }
     
+    useEffect(() => {
+    
+    }, [eliminar()]);
+
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-12"  style={estiloDiv} >
-                    <input type="text" className="text" style={estiloInput} value={props.text} onChange={(e) => props.setText(e.target.value)} onKeyPress={(props.handleAnswerChange)}/>
+                <div className="col-md-12" style={estiloDiv} >
+                    <input type="text" className="text" style={estiloInput} value={props.text} onChange={(e) => props.setText(e.target.value)} onKeyPress={(props.handleAnswerChange)} />
                     <br></br>
-                    <span style={estiloSpan}>{props.objeto}</span>
+                    {
+                        props.objeto.length > 0 &&
+                        props.objeto.map((obj, num = num+1) => (
+                            <div className="col-12" key={num} style={estiloDivMap}>
+                                <span id={num} style={estiloSpan}>{obj}</span><MdOutlineClose id={num} onClick={() => eliminar(num)} />
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -19,7 +19,6 @@ const Home = () => {
 			return
 		} else true
 	}
-	console.log(objeto)
 	const Span = () => {
 
 		return (
